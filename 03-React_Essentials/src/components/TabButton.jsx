ec8d7b6b07a10ffc38316cf4305e40e834bb9758
Stefan Markov <stef4otm@gmail.com>
2024-08-15 17:48:16 +0300

    15 08 24

--- 03-React_Essentials/src/components/TabButton.jsx
+++ 03-React_Essentials/src/components/TabButton.jsx
@@ -1,11 +1,9 @@
 export default function TabButton(props) {
-  function handleClick() {
-    console.log("Hello World :)");
-  }
-
   return (
     <li>
-      <button onClick={handleClick}>{props.children}</button>
+      <button className={props.isSelected ? "active" : ""} onClick={props.onSelect}>
+        {props.children}
+      </button>
     </li>
   );
 }
