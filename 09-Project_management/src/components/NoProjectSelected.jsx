9a2ec2c4bc7506ea073acecd64c87e61956d988e
Stefan Markov <stef4otm@gmail.com>
2024-12-05 15:24:23 +0200

    12 05 24

---
+++ 09-Project_management/src/components/NoProjectSelected.jsx
@@ -0,0 +1,12 @@
+export default function NoProjectSelected() {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img src="" alt="" />
+      <h2>No project selected</h2>
+      <p>Select a project or get started with a new one</p>
+      <p>
+        <button>Create new project</button>
+      </p>
+    </div>
+  );
+}
